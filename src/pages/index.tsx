3a6b5b74fba867a11da00878246c6c112908143e
nikitazapekin <wotblitz362@mail.ru>
2024-09-21 19:35:35 +0300

    add form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,21 @@ import Header from "@/widgets/Header/Header";
 import styles from "../app/theme/wrappers.module.scss"
 import WelcomeSection from "@/widgets/WelcomeSection/WelcomeSection";
 import MainPageSliderSection from "@/widgets/MainPageSliderSection/MainPageSliderSection";
+import AuthModal from "@/widgets/AuthModal/AuthModal";
 export default function Home() {
   return (
     <>
       <div className={styles.wrapper}>
-
+{/*
+        */}
         <Header />
         <div className={styles.content}>
           <WelcomeSection />
           <MainPageSliderSection title={"Специально для тебя"} />
+
+          <AuthModal />
+
+
         </div>
         wffw
       </div>
--- src/widgets/AuthModal/AuthModal.tsx
+++ src/widgets/AuthModal/AuthModal.tsx
@@ -1,14 +1,96 @@
 import styles from "./AuthModal.module.scss"
+import User from "@/shared/assets/icons/user.png"
+import Lock from "@/shared/assets/icons/lock.png"
+import Image from "next/image"
+import Discord from "@/shared/assets/social-networks/discord.png"
+import Vk from "@/shared/assets/social-networks/vk.png"
+import Google from "@/shared/assets/social-networks/google.png"
 const AuthModal = () => {
     return (
         <div className={styles.modal}>
-            <div className={styles.modal__inner}>
+            <form className={styles.modal__inner}>
+                <div className={styles.modal__content}>
+                    <div className={styles.modal__header}>
+                        <p className={styles.modal__sign__in}>
+                            Вход
+                        </p>
+                        <p className={styles.modal__sign__up}>
+                            Регистрация
+                        </p>
+                    </div>
+                    <p className={styles.modal__title}>
+                        Вход через никнейм или Email
+                    </p>
+                    <div className={styles.modal__field}>
+                        <Image src={User} alt="icon" className={styles.modal__icon} />
+                        <input
+                            className={styles.modal__input}
+                            type="email" placeholder="Введите никнейм или Email"
+                        />
+                    </div>
+                    <div className={styles.modal__field}>
+                        <Image src={Lock} alt="icon" className={styles.modal__icon} />
+                        <input
+                            className={styles.modal__input}
+                            type="email" placeholder="Введите пароль"
+                        />
+                    </div>
 
-            </div>
-            <div className={styles.modal__overlay}>
 
-            </div>
 
+                    <div className={styles.modal__remember}>
+                        <input type="checkbox" className={styles.modal__checkbox} />
+                        <p className={styles.modal__remember__text}>
+                            Запомнить меня
+                        </p>
+                    </div>
+
+
+                    <div className={styles.modal__line} />
+                    <button className={styles.modal__btn}>
+                        Войти
+                    </button>
+
+                    <div className={styles.modal__or}>
+                        <div className={styles.modal__or__line} />
+                        <p className={styles.modal__or__text}>
+                            Или
+                        </p>
+                        <div className={styles.modal__or__line} />
+                    </div>
+
+
+                    <p className={styles.modal__title}>
+                        Вход через социальные сети
+                    </p>
+
+
+                    <div className={styles.modal__networks}>
+
+                        <div className={`${styles.modal__network} ${styles.modal__network__discord}`}>
+                            <Image src={Discord} className={styles.network__image} alt="network" />
+                        </div>
+
+                        <div className={`${styles.modal__network} ${styles.modal__network__google}`}>
+                            <Image src={Google} className={styles.networks__image} alt="network" />
+                        </div>
+                        <div className={`${styles.modal__network} ${styles.modal__network__vk}`}>
+                            <Image src={Vk} className={styles.networks__image} alt="network" />
+                        </div>
+                    </div>
+
+                    <div className={styles.modal__footer}>
+                        <span className={styles.modal__footer__text}>
+                            Вы забыли пароль?
+                        </span>
+                        <span className={styles.modal__restore}>
+                            Восстановить
+                        </span>
+                    </div>
+
+                </div>
+            </form>
+            <div className={styles.modal__overlay} />
         </div>
     );
 }
