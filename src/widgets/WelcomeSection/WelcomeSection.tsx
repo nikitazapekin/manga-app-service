1c4f00174fb857e0712bc87ba462ef3b22f06712
nikitazapekin <wotblitz362@mail.ru>
2024-09-29 12:28:51 +0300

    fix layout of zero one

--- src/widgets/WelcomeSection/WelcomeSection.tsx
+++ src/widgets/WelcomeSection/WelcomeSection.tsx
@@ -1,38 +1,12 @@
-/*
-import SliderCarouselHeader from "@/entities/SliderCarouselHeader/SliderCarouselHeader";
-import styles from "./WelcomeSection.module.scss"
-//import Zero from "@/shared/assets/zero.svg"
-import ResearchBtn from "@/shared/ui/ResearchBtn/ResearchBtn";
-//import Image from "next/image";
-const WelcomeSection = () => {
-    return (
-        <section className={styles.welcome}>
-            <div className={styles.welcome__inner}>
-                <div className={styles.welcome__preview}>
-                    <h1 className={styles.welcome__preview__title}>
-                    Добро пожаловать 
-                    на мангалиб
-                    </h1>
-                  
-                    <button className={styles.btn}>
-                        scasc
-                    </button>
-                </div>
-            </div>
-        </section>
-    );
-}
 
-export default WelcomeSection;
-*/
 import SliderCarouselHeader from "@/entities/SliderCarouselHeader/SliderCarouselHeader";
 import styles from "./WelcomeSection.module.scss"
-//import Zero from "@/shared/assets/zero.svg"
+import Zero from "@/shared/assets/zero.png"
 import ResearchBtn from "@/shared/ui/ResearchBtn/ResearchBtn";
 import WelcomeSliderSwiper from "@/entities/WelcomeSliderSwiper/WelcomeSliderSwiper";
-//import Image from "next/image";
-import Element from "@/shared/assets/element.png"
 
+import Element from "@/shared/assets/element.png"
+import Image from "next/image";
 const elems = [
     { title: "Test", image: Element },
     { title: "Test", image: Element },
@@ -46,39 +20,42 @@ const elems = [
     { title: "Test", image: Element },
     { title: "Test", image: Element },
     { title: "Test", image: Element },
-  ];
-  
-  
+];
+
+
 const WelcomeSection = () => {
     return (
         <section className={styles.welcome}>
-      
-           <div className={styles.welcome__background}> 
-            <div className={styles.welcome__inner}>
-                <div className={styles.welcome__preview}>
-                <h1 className={styles.welcome__title}>
-                Добро пожаловать 
-                на мангалиб
-                </h1>
-                <button className={styles.btn}>
-                    Исследовать
-                    </button>
-           
-                </div>
-                <div className={styles.welcome__content}>
-{/*}
+
+            <div className={styles.welcome__background}>
+                <div className={styles.welcome__inner}>
+                    <div className={styles.welcome__preview}>
+                        <h1 className={styles.welcome__title}>
+                            Добро пожаловать
+                            на мангалиб
+                        </h1>
+                        <button className={styles.btn}>
+                            Исследовать
+                        </button>
+
+                    </div>
+                    <div className={styles.welcome__content}>
+                        {/*}
    <SliderCarouselHeader />
    */}
 
-   <WelcomeSliderSwiper items={elems} />
-                </div>
-              
-            </div>
-            </div>
-            <div className={styles.welcome__triangle} />
+                        <WelcomeSliderSwiper items={elems} />
+                    </div>
 
-           
-         
+                </div>
+<Image className={styles.welcome__zero}  src={Zero} alt="Zero one" />
+            </div>
+            <div className={styles.welcome__triangle} >
+dvs fewfew
+            </div>
+
+
+
         </section>
     );
 }
