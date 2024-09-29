6108536cfc26379147b997bdd7d2fd8f5f08f8fe
nikitazapekin <wotblitz362@mail.ru>
2024-09-29 17:00:53 +0300

    last

--- src/widgets/WelcomeSection/WelcomeSection.tsx
+++ src/widgets/WelcomeSection/WelcomeSection.tsx
@@ -23,11 +23,50 @@ const elems = [
 ];
 
 
+
+
 const WelcomeSection = () => {
     return (
         <section className={styles.welcome}>
+            <div className={styles.welcome__inner}>
 
-            <div className={styles.welcome__background}>
+                <div className={styles.welcome__content}>
+
+                    <h1 className={styles.welcome__title}>
+                        Добро пожаловать
+                        на мангалиб
+                    </h1>
+                    <button className={styles.btn}>
+                        Исследовать
+                    </button>
+                    <WelcomeSliderSwiper items={elems} />
+                </div>
+
+
+                <Image className={styles.welcome__zero} src={Zero} alt="Zero one" />
+            </div>
+
+            <div className={styles.welcome__triangle}>
+
+            </div>
+
+
+
+       
+        </section>);
+}
+
+export default WelcomeSection;
+/*
+
+const WelcomeSection = () => {
+return (
+    <section className={styles.welcome}>
+
+        <div className={styles.welcome__background}>
+
+            <div className={styles.welcome__entry}>
+
                 <div className={styles.welcome__inner}>
                     <div className={styles.welcome__preview}>
                         <h1 className={styles.welcome__title}>
@@ -40,26 +79,23 @@ const WelcomeSection = () => {
 
                     </div>
                     <div className={styles.welcome__content}>
-                        {/*}
-   <SliderCarouselHeader />
-   */}
 
                         <WelcomeSliderSwiper items={elems} />
                     </div>
 
                 </div>
-<Image className={styles.welcome__zero}  src={Zero} alt="Zero one" />
+                <Image className={styles.welcome__zero} src={Zero} alt="Zero one" />
             </div>
-            <div className={styles.welcome__triangle} >
-dvs fewfew
-            </div>
+        </div>
+        <div className={styles.welcome__triangle} >
+            dvs fewfew
+        </div>
 
 
 
-        </section>
-    );
+    </section>
+);
 }
 
 export default WelcomeSection;
-/*
 */
